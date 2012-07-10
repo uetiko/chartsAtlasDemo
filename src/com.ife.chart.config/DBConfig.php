8e62714607fb91e89d495306797c557962f591b7
Angel Barrientos <silent@uetiko.(none)>
2012-07-10 16:55:39 -0500

    archivois de configuración de la base de datos

--- src/com.ife.chart.config/DBConfig.php
+++ src/com.ife.chart.config/DBConfig.php
@@ -17,6 +17,25 @@ class DBConfig{
     public function getXbaseName(){
         return $this->properties['xbase']['name'];
     }
+    
+    public function getUsuarioPostgres(){
+        return $this->properties['base']['usuario'];
+    }
+    
+    public function getPasswdPostgres(){
+        return $this->properties['base']['passwd'];
+    }
+    
+    public function getHostPostgres(){
+        return $this->properties['base']['host'];
+    }
+    
+    public function getPuertoPostgres(){
+        return $this->properties['base']['puerto'];
+    }
+    
+    public function getBasePostgres(){
+        return $this->properties['base']['base'];
+    }
 }
-
 ?>
