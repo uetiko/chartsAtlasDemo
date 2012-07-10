11cbb60c5b99fd53b9891b88324f3aae1f8542b0
Angel Barrientos <silent@uetiko.(none)>
2012-07-10 16:55:03 -0500

    cambios en el action

--- src/com.ife.chart.action/PeticionAction.php
+++ src/com.ife.chart.action/PeticionAction.php
@@ -1,9 +1,13 @@
 <?php
 include_once '../com.ife.chart.vo/ProcesaVO.php';
 include_once '../com.ife.chart.lib/JSON.php';
+include_once '../com.ife.chart.utils/JJUtils.php';
 
 /**
- * 
+ * @version 0.1
+ * @author Angel Barrientos Cruz uetiko@gmail.com
+ * @package com.ife.chart.action
+ * @license Resevada para el Instituto Federal Electoral
  */
 class PeticionAction{
 	private $json;
@@ -20,6 +24,20 @@ class PeticionAction{
         $vo = new ProcesaVO();
         $d = array('datos' => $vo->getPaisDatos($idPais), 'estado' => $vo->getOnlyEstados(), 'numeros' => $vo->getDatosEstados($idPais));
         return $this->json->encode($d);
+    }
+    
+    public function executeGetFecha(){
+        $utils = new JJUtils();
+        $array = array('fecha' => $utils->getFechaActual());
+        return $this->json->encode($array);
+    }
+    
+    public function executeMapLink(){
+        $vo = new ProcesaVO();
+        $array = $vo->procesaLinkMap();
+        //print_r($array);
+        $format = array('nombre' => $array[0]['nombreCorto'], 'link' => $array[0]['link']);
+        return $this->json->encode($format);
     }
 }
 
--- src/com.ife.chart.action/test.php
+++ src/com.ife.chart.action/test.php
@@ -3,5 +3,6 @@ include_once '../com.ife.chart.action/PeticionAction.php';
 
 $action = new PeticionAction();
 echo $action->executeGetPais() . "\n\n\n\n\n\n\n\n";
-echo $action->executePaisData('29') . "\n";
+echo $action->executePaisData('29') . "\n\n\n\n\n";
+echo $action->executeMapLink() . "\n";
 ?>
