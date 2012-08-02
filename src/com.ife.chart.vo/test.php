f30415771d603428cd46fddb603864cd40bc5b4d
Angel Barrientos <silent@uetiko.gateway.2wire.net>
2012-08-02 01:17:33 -0500

    script test para vo

--- src/com.ife.chart.vo/test.php
+++ src/com.ife.chart.vo/test.php
@@ -1,8 +1,11 @@
 <?php
 include_once '../com.ife.chart.utils/JJUtils.php';
+include_once '../com.ife.chart.vo/ProcesaVO.php';
 
 $test = new JJUtils();
+$pvo = new ProcesaVO();
 
 echo $test->getFechaActual() . "\n";
+echo $pvo->procesaDFB01Pais();
 
 ?>
