d7681b4a63eceea49becdc5b06c78203d19a43a9
Angel Barrientos <silent@uetiko.(none)>
2012-08-09 19:29:07 -0500

    script de pruebas.
    Se implemento la clase estatica GraficaTipoUtils.

--- src/com.ife.chart.vo/test.php
+++ src/com.ife.chart.vo/test.php
@@ -1,12 +1,24 @@
 <?php
 include_once '../com.ife.chart.utils/JJUtils.php';
 include_once '../com.ife.chart.vo/ProcesaVO.php';
+include_once '../com.ife.chart.utils/GraficaTipoUtils.php';
+include_once '../com.ife.chart.lib/JSON.php';
 
 $test = new JJUtils();
 $pvo = new ProcesaVO();
 
 echo $test->getFechaActual() . "\n";
-echo 
-print_r($pvo->procesaDBFMes("DIC"));
+//print_r($pvo->procesaDBFMes("octMay"));
+print_r(GraficaTipoUtils::tagsGraficas("hola", "subNombre", "ejeY", "ejeX", "unidadesY", "unidadesX", "valNombre1", "valNombre2"));
+$json = new Services_JSON();
+echo "\n\n";
+$c = GraficaTipoUtils::tagsGraficas("hola", "subNombre", "ejeY", "ejeX", "unidadesY", "unidadesX", "valNombre1", "valNombre2");
+$d = array();
+foreach ($c as $key) {
+	foreach ($key as $value) {
+		$d[] = $value;
+	}
+}
+echo $json->encode($d);
 
 ?>
