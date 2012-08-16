3a40ea46c3a087dbd6c46a62883ee0aea4ca592c
Angel Barrientos <silent@uetiko.(none)>
2012-08-16 18:22:54 -0500

    cambios menores

--- src/com.ife.chart.dao/QueryDAO.php
+++ src/com.ife.chart.dao/QueryDAO.php
@@ -10,46 +10,50 @@ include_once '../com.ife.chart.config/DBConfig.php';
  * @license http://www.gnu.org/copyleft/lesser.html Distributed under the Lesser General Public License (LGPL)
  * @copyright 2012
  */
-class QueryDAO{
-	private $cnn;
+class QueryDAO {
+    private $cnn;
     private $conf;
-	function __construct() {
-	    $this->conf = new DBConfig();
-		$this->cnn = new Connection($this->conf->getUsuarioPostgres(), $this->conf->getPasswdPostgres(), $this->conf->getHostPostgres(), $this->conf->getPuertoPostgres(), $this->conf->getBasePostgres());
-	}
-    
-    public function getLinks(){
-        $this->cnn->openPersistentPgConnection();
+    function __construct() {
+        $this -> conf = new DBConfig();
+        $this -> cnn = new Connection($this -> conf -> getUsuarioPostgres(), $this -> conf -> getPasswdPostgres(), $this -> conf -> getHostPostgres(), $this -> conf -> getPuertoPostgres(), $this -> conf -> getBasePostgres());
+    }
+
+    public function getLinks() {
+        $this -> cnn -> openPersistentPgConnection();
         $data = array();
         $query = 'select nombre, link, "nombreCorto" from tbl_links';
-        $result = pg_query($this->cnn->getPgConnection(), $query);
+        $result = pg_query($this -> cnn -> getPgConnection(), $query);
         while ($row = pg_fetch_array($result)) {
             $data[] = $row;
         }
         return $data;
     }
 
-    public function getMapas(){
+    public function getMapas() {
         $array = array();
-        $this->cnn->openPersistentPgConnection();
+        $this -> cnn -> openPersistentPgConnection();
         $select = "select * from tbl_mapas";
-        $result = pg_query($this->cnn->getPgConnection(), $select);
+        $result = pg_query($this -> cnn -> getPgConnection(), $select);
         while ($row = pg_fetch_array($result)) {
             $array[] = $row;
         }
         return $array;
     }
-    
-    public function getMapaPath($id){
-        $this->cnn->openPersistentPgConnection();
+
+    public function getMapaPath($id) {
+        $this -> cnn -> openPersistentPgConnection();
         $d = array();
         $query = "select * from tbl_mapas where mapa_id = '" . $id . "'";
-        $result = pg_query($this->cnn->getPgConnection(), $query);
-        while ($row = pg_fetch_assoc($result)) {
-            $d[] = $row;
+        $result = pg_query($this -> cnn -> getPgConnection(), $query);
+        try {
+            while ($row = pg_fetch_assoc($result)) {
+                $d[] = $row;
+            }
+        } catch(exception $e) {
+            return array('error' => $e -> getTrace());
         }
         return $d;
     }
-}
 
+}
 ?>
